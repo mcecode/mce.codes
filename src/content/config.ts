fc07b8ea8ca8c0f68226d8a389b2f4c704eecb61
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2023-06-15 01:36:47 +0800

    Setup notes collection

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from "astro:content";
+
+export const collections = {
+  notes: defineCollection({
+    type: "content",
+    schema: z
+      .object({
+        title: z.string(),
+        dateCreated: z.date(),
+        dateUpdated: z.date()
+      })
+      .required()
+      .strict()
+  })
+};
