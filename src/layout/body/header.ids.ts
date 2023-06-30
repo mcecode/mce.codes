bee3057a6895d33899d2896d3697dd361d0638dd
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2023-07-01 05:08:36 +0800

    Update header
    - Refactor
    - Change mobile design
    - Add notes page to navigation

--- src/layout/body/header.ids.ts
+++ src/layout/body/header.ids.ts
@@ -1,3 +1,16 @@
+export const headerId = "";
+
 export const logoTitleId = "";
+
+export const navId = "";
+export const navListId = "";
+
+export const menuTemplateId = "";
+export const menuButtonId = "";
+export const menuTitleId = "";
+
+export const modeTemplateId = "";
 export const modeButtonId = "";
 export const modeTitleId = "";
+export const modeMoonId = "";
+export const modeSunId = "";
