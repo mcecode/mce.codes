a984ef0006e8b4c51e029c44f5b67cb7c39224c8
Matthew Espino <65783406+mcecode@users.noreply.github.com>
2023-07-05 00:41:36 +0800

    Refactor header

---
+++ src/lib/store.ts
@@ -0,0 +1,15 @@
+export default class Store<T extends string> {
+  #key: string;
+
+  constructor(key: string) {
+    this.#key = key;
+  }
+
+  get(): T | null {
+    return localStorage.getItem(this.#key) as T | null;
+  }
+
+  set(value: T) {
+    localStorage.setItem(this.#key, value);
+  }
+}
